fc09d047c5d7a64228c575c081c90c14a98ba59a
kyj0101 <fioonmp1001@gmail.com>
2024-04-01 01:28:49 +0900

    init

---
+++ src/test/java/com/chatappdemo/ChatAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
