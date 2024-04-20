bb79e9ac1126b4f59a7aab11551851ab8202b9b4
kyj0101 <fioonmp1001@gmail.com>
2024-04-21 01:34:19 +0900

    page controller 추가

--- src/main/java/com/chatappdemo/version1/controller/ChatController.java
+++ src/main/java/com/chatappdemo/version1/controller/ChatController.java
@@ -1,9 +1,12 @@
-package com.chatappdemo.controller;
+package com.chatappdemo.version1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequestMapping("/version1")
 public class ChatController {
     // 채팅 보내주기 sessionId
     // 채팅 조회 sessiondId
@@ -14,6 +17,11 @@ public class ChatController {
 
     // h2 member
     // chat mongodb
+
+    @GetMapping("/page")
+    public String chatPage() {
+        return "/version1/chat";
+    }
 
 
 }
